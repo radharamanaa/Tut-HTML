7595ac91b7006be327866398d8a5ac83eb786f58
Abhijeet Mishra <colouredpages@gmail.com>
2022-11-17 21:40:01 +0530

    first & second class

---
+++ flex-class/index.js
@@ -0,0 +1,17 @@
+let count = 1;
+count = "";
+count = 5.95;
+
+
+const var2 = 2;
+
+// var2 = 4;
+
+console.log(var2);
+
+let arrayData = document.getElementsByClassName("items");
+
+for(let i=0;i<arrayData.length;i++){
+    console.log(arrayData[i].textContent);
+    arrayData[i].textContent = "Hellooooooooooooooooooooooooo";
+}
